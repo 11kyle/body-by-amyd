de850a7536cb6ee90ecec86b7dc33479c9e924bf
Kyle <11kyle@users.noreply.github.com>
2018-12-10 15:21:15 -0700

    Content update

--- src/Footer.js
+++ src/Footer.js
@@ -10,41 +10,21 @@ class Footer extends Component {
             <small className="d-block mb-3 text-muted">&copy; 2017-2018</small>
           </div>
           <div className="col-6 col-md">
-            <h5>Features</h5>
+            <h5>Navigation</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Cool stuff</a></li>
-              <li><a className="text-muted" href="#">Random feature</a></li>
-              <li><a className="text-muted" href="#">Team feature</a></li>
-              <li><a className="text-muted" href="#">Stuff for developers</a></li>
-              <li><a className="text-muted" href="#">Another one</a></li>
-              <li><a className="text-muted" href="#">Last time</a></li>
-            </ul>
-          </div>
-          <div className="col-6 col-md">
-            <h5>Resources</h5>
-            <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Resource</a></li>
-              <li><a className="text-muted" href="#">Resource name</a></li>
-              <li><a className="text-muted" href="#">Another resource</a></li>
-              <li><a className="text-muted" href="#">Final resource</a></li>
-            </ul>
-          </div>
-          <div className="col-6 col-md">
-            <h5>Resources</h5>
-            <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Business</a></li>
-              <li><a className="text-muted" href="#">Education</a></li>
-              <li><a className="text-muted" href="#">Government</a></li>
-              <li><a className="text-muted" href="#">Gaming</a></li>
+              <li><a className="text-muted" href="#">Home</a></li>
+              <li><a className="text-muted" href="#">Pricing</a></li>
+              <li><a className="text-muted" href="#">Testimonials</a></li>
+              <li><a className="text-muted" href="#">Blog</a></li>
+              <li><a className="text-muted" href="#">About</a></li>
             </ul>
           </div>
           <div className="col-6 col-md">
             <h5>About</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Team</a></li>
-              <li><a className="text-muted" href="#">Locations</a></li>
-              <li><a className="text-muted" href="#">Privacy</a></li>
-              <li><a className="text-muted" href="#">Terms</a></li>
+              <li><a className="text-muted" href="#">4745 Independence Street,</a></li>
+              <li><a className="text-muted" href="#">Wheat Ridge, CO 80033</a></li>
+              <li><a className="text-muted" href="#">2018 Body by Amy, LLC</a></li>
             </ul>
           </div>
         </div>
--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -5,8 +5,8 @@ class Jumbotron extends Component {
     return (
       <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div className="col-md-5 p-lg-5 mx-auto my-5">
-          <h1 className="display-4 font-weight-normal">Punny headline</h1>
-          <p className="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
+          <h1 className="display-4 font-weight-normal">Body by Amy</h1>
+          <p className="lead font-weight-normal">Hi! Thanks so much for stopping by! I'm Amy, your personal trainer, running coach and fitness nutrition specialist. I am certified for personal training through the American Council on Exercise and for coaching runners throught the Run Right Pro certification program. I hold additional certifications in fitness nutrition and functional training. I have been coaching and training for nearly four years and running for over a decade. Being fit and active is truly a way of life! It has made me happier and healthier and it can do the same for you.</p>
           <a className="btn btn-outline-secondary" href="#">Coming soon</a>
         </div>
         <div className="product-device shadow-sm d-none d-md-block"></div>
